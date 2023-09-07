6fce7036bd5d2856a854acd9134a2f3cce930a45
Ka1d000 <Eugenevoiteshik@gmail.com>
2023-09-07 17:13:40 +0100

    Added Entities

---
+++ server/RAFWEB.Data/Models/ContactInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace RAFWEB.Data.Models
+{
+    public class ContactInfo : BaseEntity
+    {
+        public string Text { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ server/RAFWEB.Data/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace RAFWEB.Data.Models
+{
+    public class User : BaseEntity
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public UserRole Role { get; set; }
+        public string Image { get; set; }
+        public string About { get; set; }
+    }
+}
